8c8a01198485321d1f41c5c344390ae3d9705383
Eric Hale <eric_hale@comcast.net>
2021-10-29 21:59:17 -0600

    add comments

--- mexican_wave/lib/wave.rb
+++ mexican_wave/lib/wave.rb
@@ -4,8 +4,11 @@ class Wave
         i = 0
         # i keeps track of our position in the word and serves to end our "while" loop
         while i < str.length 
+            # guard statement to prevent 'non-alphabet' characters from capitalization
             if ('a'..'z').include?(str[i])
+                # rotates characters until the one we need uppercase is first in array
                 upcased = str.chars.rotate(i).join.capitalize
+                # rotates characters back to original position and pushes to wave
                 wave <<  upcased.chars.rotate(-i).join
             end
             i += 1
