734b4da04418aeb1b79a123472ded40573983afc
Eric Hale <eric_hale@comcast.net>
2021-10-31 20:04:48 -0600

    solution 2

--- +1_array/lib/add_one.rb
+++ +1_array/lib/add_one.rb
@@ -1,8 +1,7 @@
 class AddOne
     def up_array(arr)
-        return nil if arr.empty? || arr.any? {|num| num.negative?}
-        one_up = arr.join.to_i + 1
-        one_up.to_s.chars.map do |num|
+        return nil if arr.empty? || arr.any? {|num| num > 9 || num < 0}
+        arr.join.next.chars.map do |num|
             num.to_i
         end
     end
--- +1_array/test/add_one_test.rb
+++ +1_array/test/add_one_test.rb
@@ -12,6 +12,7 @@ class CalculatingFunctionsTest < MiniTest::Test
     def test_it_counts_chars
         assert_equal [2,4,0], @array.up_array([2,3,9])
         assert_equal [4,3,2,6], @array.up_array([4,3,2,5])
+        assert_nil @array.up_array([1,2,3,33])
         assert_nil @array.up_array([1,-9])
         assert_nil @array.up_array([])
     end
