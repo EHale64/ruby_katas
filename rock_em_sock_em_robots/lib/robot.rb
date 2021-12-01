4b8645b993c3f88f886460519733b1eb7aef31e6
Eric Hale <eric_hale@comcast.net>
2021-12-01 10:16:29 -0700

    add fighting methods

--- rock_em_sock_em_robots/lib/robot.rb
+++ rock_em_sock_em_robots/lib/robot.rb
@@ -11,13 +11,20 @@ class Robot
         @health -= num
     end
 
-    def attack(target, tactics)
-        target.hit(tactics[self.tactics[0]])
+    def attack(target, tactics, i=0)
+        target.hit(tactics[self.tactics[i]]) 
     end
 end
 
 def fight(robot_1, robot_2, tactics)
-    binding.pry
+    order = turn_order([robot_1, robot_2])
+    i = 0
+    until i == order.max_by(&:tactics).tactics.size || robot_2.health <= 0 || robot_1.health <=0
+        order[0].attack(order[1], tactics, i) 
+        order[1].attack(order[0], tactics, i) if order[1].health > 0 
+        i += 1
+    end
+    "#{order.max_by(&:health).name} has won the fight."
 end
 
 def turn_order(array)
